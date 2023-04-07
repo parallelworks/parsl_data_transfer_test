1d034883ab1b239ec98665f276c5845d924ca3eb
avidalto <avidalto@hawk.iit.edu>
2023-04-07 08:17:28 -0500

    Clone the latest dev branch for parsl_utils

--- parsl_wrapper.sh
+++ parsl_wrapper.sh
@@ -3,6 +3,6 @@ set -x
 
 # Otherwise the submodule is fixed to a given commit...
 rm -rf parsl_utils
-git clone https://github.com/parallelworks/parsl_utils.git parsl_utils
+git clone -b alvaro/new-slurmshv2 https://github.com/parallelworks/parsl_utils.git parsl_utils
 # Cant run a scripts inside parsl_utils directly
 bash parsl_utils/main.sh $@
